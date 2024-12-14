8e0ce68ce743881b294aaf477ef26b66a8c97732
panyfmc <paulavanessa010@gmail.com>
2024-12-14 00:11:46 -0300

    :bulb: feat: comentários para melhor leitura do código

--- lista2/q4.js
+++ lista2/q4.js
@@ -1,12 +1,13 @@
-//Programa para classificar um triângulo em Equilátero, Isósceles ou Escaleno a partir dos valores de seus três lados
+//Programa para classificar um triângulo em Equilátero, Isósceles ou
+// Escaleno a partir dos valores de seus três lados
 
 
 function classTriangulo(l1, l2, l3) {
-    if (l1 == l2 && l2 == l3) {   
+    if (l1 == l2 && l2 == l3) {             // Três lados iguais
         return "Triângulo Equilátero"
-    } else if ((l1 == l2) || (l2 == l3) || (l1 == l3)) {
+    } else if ((l1 == l2) || (l2 == l3) || (l1 == l3)) {        // Dois lados iguais
         return "Triângulo Isósceles"
-    } else if ((l1 != l2) && (l1 != l3) && (l2 != l3)) {
+    } else if ((l1 != l2) && (l1 != l3) && (l2 != l3)) {        // Lados diferentes
         return "Triângulo Escaleno"
     } 
 }
