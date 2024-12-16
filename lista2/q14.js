c3d9886e8e6467e8d2014c9f2eade3d4cd0e7a13
panyfmc <paulavanessa010@gmail.com>
2024-12-15 21:47:52 -0300

    :bulb: docs: comentários para melhor compreensão da lógica aplicada na questão

--- lista2/q14.js
+++ lista2/q14.js
@@ -1,15 +1,18 @@
 // Programa que escreva por extenso um determinado algarismo passado como argumento.
 
 function porExtenso () {
-    const algarismo = Number(prompt(`digite o algarismo que deseja formatar por extenso: `))
+    const algarismo = Number(prompt(`digite o algarismo que deseja formatar por extenso: `))   //Receber o número (0-9)
+    // Lista com todos os algarismos escritos por extenso que serão utilizados no programa
     const numerosExtenso = [
         "zero", "um", "dois", "três", "quatro", 
         "cinco", "seis", "sete", "oito", "nove"
     ]
 
     if (algarismo >= 0 && algarismo <= 9) {
+        // Caso o número esteja entre 0 e 9
         return numerosExtenso(algarismo)
     } else {
+        // Números diferentes de 0 à 9
         return `Algarismo fora do intervalo permitido (0-9)`
     }
 }
