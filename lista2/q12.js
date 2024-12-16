b29336ba748443a29f302b501a9b03eccae284aa
panyfmc <paulavanessa010@gmail.com>
2024-12-15 21:41:53 -0300

    :bulb: docs: comentários para melhor compreensão da lógica aplicada na função

--- lista2/q12.js
+++ lista2/q12.js
@@ -1,5 +1,5 @@
 // Escreva um programa para calcular o maior e o menor valor real das raízes de uma equação
-// de segundo grau. A expressão genérica para cálculo das raízes é dada por baskhara 
+// de segundo grau. A expressão genérica para cálculo das raízes é dada por bhaskara 
 
 // -b +- sqrt b^2 - 4 a c / 2a
 
@@ -8,13 +8,19 @@ function valordasRaizes() {
     const a = Number(prompt('Digite o valor de a: '))
     const b = Number(prompt('Digite o valor de b: '))
     const c = Number(prompt('Digite o valor de c: '))
-    const delta = (b ** 2) - 4 * a * c
+
+    const delta = (b ** 2) - 4 * a * c      // Calcular o delta
     if (delta < 0) {
+        // Se o valor do delta for menor que 0
         return 'Não há raízes reais.'
     } else {
-        const x1 = (-b + Math.sqrt(delta)) / (2 * a)
-        const x2 = (-b - Math.sqrt(delta)) / (2 * a) 
+        // Se o delta for >= 0 existe raíz real e pode aplicar a fórmula de bhaskara
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a)        // Valor do x1 (+)
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a)        // Valor do x2 (-)
+
+        // Verificar qual é a raíz maior e menor entre x1 e x2
         return `valor maior: ${Math.max(x1, x2)}, valor menor: ${Math.min(x1, x2)}`
+        
     }
 
 }
