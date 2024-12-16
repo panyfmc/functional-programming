e01401ae9a6b905395c06c621321bb3a63721cd7
panyfmc <paulavanessa010@gmail.com>
2024-12-15 21:08:35 -0300

    :bulb: docs: comentários para compreender melhor a lógica na função

--- lista2/q7.js
+++ lista2/q7.js
@@ -3,14 +3,14 @@
 // iguais, deve-se retornar como resultado o próprio valor).
 
 function menorValor(a, b, c) {
-    if (a < b && a < c) {
+    if (a < b && a < c) {           // Se a for menor que b e c
         return `O menor número é: ${a}`
-    } else if (b < c) {
+    } else if (b < c) {             //  Se a é maior que b e c, então se b < c
         return `O menor número é: ${b}`
-    } else if (a == b && a == c) {
+    } else if (a == b && a == c) {  // Se a, b e c são iguais
         return `Os números são iguais: ${a}`
-    } else {
-        return `O menor número é: ${c}`
+    } else {                        // Se b não é menor que c, então
+        return `O menor número é: ${c}` 
     }
 
 }
