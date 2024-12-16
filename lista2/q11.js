44b2469945b59a6346274b16c75074285e438816
panyfmc <paulavanessa010@gmail.com>
2024-12-15 21:37:17 -0300

    :recycle: refactor: interpretação ambígua na 2 condicional

--- lista2/q11.js
+++ lista2/q11.js
@@ -6,13 +6,22 @@ function maiorValor() {
     const b = prompt('Digite outro valor: ')
     const c = prompt('Digite um último valor: ')
     const media = (a + b + c)/3
-    if (a > media && b > media && c > media) {
+    if (a > media && b > media && c > media) {      
+        // Se a, b e c forem (individualmente) maiores que a média deles
         return '3'
-    } else if ((a < media) || (b < media) || (c < media)) {
+
+    } else if (
+        (a > media && b > media && c <= media) || 
+        (a > media && c > media && b <= media) || 
+        (b > media && c > media && a <= media)) { 
+        // Se exatamente dois valores forem meaiores que a média
         return '2'
-    } else {
+        
+    } else {        
+        // Caso contrário, se nenhum ou apenas um valor for menor que a média
         return '1'
     }
+
 }
 
 console.log(`\nQuantidade de valores maiores que a média entre eles: ${maiorValor()}`)
