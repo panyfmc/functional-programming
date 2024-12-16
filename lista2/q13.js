9a5021668772fa498321187e1db0cd09fb7124d9
panyfmc <paulavanessa010@gmail.com>
2024-12-15 21:43:53 -0300

    :bulb: docs: comentários para melhor compreensão da lógica aplicada na função

--- lista2/q13.js
+++ lista2/q13.js
@@ -2,13 +2,15 @@
 //  Considere t0 = 0, o instante inicial e x0=500 a posição inicial.
 // Escreva um programa para calcular a velocidade do objeto em um dado instante t e posição x.
 
-// s = s0 + vt
-// v = (s - s0)/t
+// s = s0 + vt  (fórmula do MU)
+
 
 function velocidadedoObjeto() {
     const s0 = 500
     const t = prompt('Digite o instante do objeto (s): ')
     const s = prompt('Digite a posição do objeto (m): ')
+
+    // v = (s - s0)/t - t0          t0 = 0
     return (s - s0)/t
 }
 
