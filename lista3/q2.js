459bf4771d9ffe292130b4b02f2856aa26190445
panyfmc <paulavanessa010@gmail.com>
2025-01-18 02:01:18 -0300

    :recycle: refactor: comentário no código para entender fluxo dos valores de entrada

--- lista3/q2.js
+++ lista3/q2.js
@@ -4,6 +4,6 @@ const areaCirc = (r, pi = 3.14) => pi*r**2
 
 const raio = parseFloat(prompt('Digite o valor do raio: ')) 
 
-const area = areaCirc(raio)
+const area = areaCirc(raio)     // O valor de raio será atribuido ao primeiro parâmetro de areaCirc, no caso, r
 
 console.log(area)
