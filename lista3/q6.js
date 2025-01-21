5ba8489c59f95a807efe325d31fef8d2a1182df3
Allan Victor <paulavanessa010@gmail.com>
2025-01-21 15:56:12 -0300

    :broom: cleanup: comentarios desnecessarios

--- lista3/q6.js
+++ lista3/q6.js
@@ -1,8 +1,6 @@
 // Q6. Fornecidos três valores, a, b e c, escreva um programa que retorne quantos dos três são iguais. A resposta pode ser 3 (todos iguais), 2 (apenas um diferente) ou 0 (todos diferentes).
 
 
-// Se acontecer isso ':',  me retorne isso '?'
-
 const comparacao = (a, b, c) => a == b && b == c ? "3 (Todos iguais)" : a != b && b != c && a != c ? "0 (Todos diferentes)" : "2 (Apenas um diferente)"
 
 // Usuário inserir os valores no terminal
