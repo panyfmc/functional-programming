7eff9af4eb069f580672e1938130d21f8bab04c1
Allan Victor <limaallan260@gmail.com>
2025-01-21 15:42:35 -0300

    :sparkles: feat: comparação entre 3 valores utilizando os conceitos da notação arrow e funcao anônima (surpresa)

---
+++ lista3/q6.js
@@ -0,0 +1,16 @@
+// Q6. Fornecidos três valores, a, b e c, escreva um programa que retorne quantos dos três são iguais. A resposta pode ser 3 (todos iguais), 2 (apenas um diferente) ou 0 (todos diferentes).
+
+
+// Se acontecer isso ':',  me retorne isso '?'
+
+const comparacao = (a, b, c) => a == b && b == c ? "3 (Todos iguais)" : a != b && b != c && a != c ? "0 (Todos diferentes)" : "2 (Apenas um diferente)"
+
+// Usuário inserir os valores no terminal
+const a1 = parseFloat(prompt('Digite o valor de a: '))
+const b1 = parseFloat(prompt('Digite ovalor de b: '))
+const c1 = parseFloat(prompt('Digite o valor de c: '))
+
+const checkTresValores = comparacao(a1, b1, c1)
+
+
+console.log(checkTresValores)
