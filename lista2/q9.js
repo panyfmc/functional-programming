621248826c799901e91809425fc8b1bd399864e1
panyfmc <paulavanessa010@gmail.com>
2025-02-01 13:49:01 -0300

    :bug: fix: correção de código do "ou-exclusivo"

--- lista2/q9.js
+++ lista2/q9.js
@@ -1,16 +1,17 @@
 //Programa que calcula o "ou-exclusivo" entre dois valores-verdade (verdadeiro ou falso),
 // dado por a⊗b=(a∨b) ∧ ¬(a∧b)
 
-// A sua Saída é 1 se e somente se apenas uma das suas Entradas é 1
 
 function verifyXor(a, b) {
-    //  as condições precisam ser verdadeiras simultaneamente
 
-    if ((a || b) && ~(a && b)) {    // a ou b é V && não ambos são verdadeiros ~(0 && 0)
+    if ((a || b) && !(a && b)) {    // a ou b é V && não ambos são verdadeiros ~(0 && 0)
         return true
     } else {
         return false
     }
 }
 
-console.log(`Valor lógico desse programa: ${verifyXor(null, 2)}`)
+const value1 = prompt("Insira o primeiro valor (true ou false): ").toLowerCase() === "true"      // Só é true se digitar TRUE
+const value2 = prompt("Insira o segundo valor (true ou false): ").toLowerCase() === "true"       // Qualquer outro é FALSE
+
+console.log(`XOR(${value1}, ${value2}) = ${verifyXor()}`)
