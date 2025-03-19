3be5803b358c026971e4e79955eed3552d1b814e
panyfmc <paulavanessa010@gmail.com>
2025-03-19 16:52:24 -0300

    :wastebasket: remove: questao nao feita

--- lista5/lista4/q4.js
+++
@@ -1,8 +0,0 @@
-//  Potência natural do número 2: 2^n . Naturalmente, você não deve utilizar o operador de expoente da linguagem.
-
-const fatorial = (n) => (n==1) ? 1 : n*fatorial(n-1)
-
-
-const num = 10
-
-console.log(fatorial(num))
